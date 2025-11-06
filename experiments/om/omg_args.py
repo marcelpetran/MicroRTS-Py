c69fd5ca8deb6514f7d57211ab4dbfc4b874599b
petram21 <petram21@fel.cvut.cz>
2025-11-06 12:51:17 +0100

    args

--- experiments/om/omg_args.py
+++ experiments/om/omg_args.py
@@ -31,7 +31,7 @@ class OMGArgs:
   selector_mode: str = "conservative"
   selector_tau_start: float = 1.0
   selector_tau_end: float = 0.1
-  selector_tau_decay_steps: int = 50_000
+  selector_tau_decay_steps: int = 150_000
   train_vae: bool = True
   vae_lr: float = 1e-4
   cvae_lr: float = 3e-4
@@ -39,7 +39,7 @@ class OMGArgs:
   # Transformer architecture params
   beta_start: float = 0.0  # Weight for CVAE KL loss
   beta_end: float = 2.0
-  beta_decay_steps: int = 70_000
+  beta_decay_steps: int = 150_000
   vae_beta: float = 0.1  # Weight for VAE KL loss
   state_shape: Tuple[int, int, int] = None  # (H, W, F)
   H: int = 5  # grid height
