f98ea67a777fcb563dc256ccc4efb2a5594ac570
petram21 <petram21@fel.cvut.cz>
2025-11-16 00:19:04 +0100

    updated params for experiments

--- experiments/om/omg_args.py
+++ experiments/om/omg_args.py
@@ -25,13 +25,13 @@ class OMGArgs:
   # OMG-specific
   gmix_eps_start: float = 1.0      # Eq.(8): start using mostly g_bar
   gmix_eps_end: float = 0.0        # goes to 0 -> use g_hat only
-  gmix_eps_decay_steps: int = 200_000
+  gmix_eps_decay_steps: int = 800_000
   horizon_H: int = 6
   # "conservative" => Eq.(7), "optimistic" => Eq.(6)
   selector_mode: str = "conservative"
   selector_tau_start: float = 20.0
   selector_tau_end: float = 0.1
-  selector_tau_decay_steps: int = 600_000
+  selector_tau_decay_steps: int = 800_000
   train_vae: bool = True
   vae_lr: float = 1e-4
   cvae_lr: float = 1e-4
@@ -39,7 +39,7 @@ class OMGArgs:
   # Transformer architecture params
   beta_start: float = 0.1  # Weight for CVAE KL loss
   beta_end: float = 2.0
-  beta_decay_steps: int = 200_000
+  beta_decay_steps: int = 800_000
   vae_beta: float = 0.1  # Weight for VAE KL loss
   state_shape: Tuple[int, int, int] = None  # (H, W, F)
   H: int = 7  # grid height
