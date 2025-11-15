5a839f4574ec5bdc5363fb10aa49f683ced8cf8e
petram21 <petram21@fel.cvut.cz>
2025-11-16 00:01:49 +0100

    refactor: update OMGArgs parameters for improved performance

--- experiments/om/omg_args.py
+++ experiments/om/omg_args.py
@@ -5,10 +5,10 @@ from typing import Tuple
 @dataclass
 class OMGArgs:
   device: str = "cpu"  # "cpu" or "cuda"
-  gamma: float = 0.99
-  lr: float = 2.5e-4
+  gamma: float = 1.0
+  lr: float = 3e-4
   batch_size: int = 16
-  capacity: int = 50_000
+  capacity: int = 250_000
   min_replay: int = 1_000
   train_every: int = 4
   target_update_every: int = 1_000
@@ -18,7 +18,7 @@ class OMGArgs:
   eps_decay_steps: int = 50_000
   qnet_hidden: int = 256
   max_history_length: int = 100
-  max_steps: int = 50
+  max_steps: int = 30
   seed: int = 0
   folder_id: int = 0
 
@@ -26,33 +26,33 @@ class OMGArgs:
   gmix_eps_start: float = 1.0      # Eq.(8): start using mostly g_bar
   gmix_eps_end: float = 0.0        # goes to 0 -> use g_hat only
   gmix_eps_decay_steps: int = 200_000
-  horizon_H: int = 3
+  horizon_H: int = 6
   # "conservative" => Eq.(7), "optimistic" => Eq.(6)
   selector_mode: str = "conservative"
-  selector_tau_start: float = 1.0
+  selector_tau_start: float = 20.0
   selector_tau_end: float = 0.1
-  selector_tau_decay_steps: int = 150_000
+  selector_tau_decay_steps: int = 600_000
   train_vae: bool = True
   vae_lr: float = 1e-4
   cvae_lr: float = 1e-4
 
   # Transformer architecture params
-  beta_start: float = 1.0  # Weight for CVAE KL loss
+  beta_start: float = 0.1  # Weight for CVAE KL loss
   beta_end: float = 2.0
-  beta_decay_steps: int = 150_000
+  beta_decay_steps: int = 200_000
   vae_beta: float = 0.1  # Weight for VAE KL loss
   state_shape: Tuple[int, int, int] = None  # (H, W, F)
-  H: int = 5  # grid height
-  W: int = 5  # grid width
+  H: int = 7  # grid height
+  W: int = 7  # grid width
   state_feature_splits: Tuple[int, ...] = ()
   # for DiscreteActionEmbedder - !at least one mustn't be None!
-  action_dim: int = None
+  action_dim: int = 4
   # for ActionEmbeddings - !at least one mustn't be None!
   action_feature_splits: Tuple[int, ...] = None
   latent_dim: int = 32
   d_model: int = 256
-  nhead: int = 8
-  num_encoder_layers: int = 2
-  num_decoder_layers: int = 2
+  nhead: int = 4
+  num_encoder_layers: int = 1
+  num_decoder_layers: int = 1
   dim_feedforward: int = 1024
   dropout: float = 0.1
