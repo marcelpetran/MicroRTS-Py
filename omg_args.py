362de4ba9891fa6c79981d0cf0511d602e44ce17
marcelpetran <marcelpetran55@gmail.com>
2026-02-23 03:04:25 +0100

    Refactor OMGArgs: remove unused parameters, streamline configuration, and improve clarity

--- omg_args.py
+++ omg_args.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
 from typing import Tuple
 
-from sympy import beta
-
 
 @dataclass
 class OMGArgs:
@@ -14,40 +12,34 @@ class OMGArgs:
   min_replay: int = 1_000
   train_every: int = 4
   target_update_every: int = 1_000
-  tau_soft: float = 0.005
   visualise_every_n_step: int = 3
-  eps_start: float = 1.0
-  eps_end: float = 0.05
-  eps_decay_steps: int = 50_000
   qnet_hidden: int = 256
-  max_history_length: int = 100
+  max_history_length: int = 31
   max_steps: int = 30
   seed: int = 0
   folder_id: int = 0
 
-  # OMG-specific
   oracle: bool = False
+  tau_soft: float = 0.005
   tau_start: float = 2.0
   tau_end: float = 0.01
   tau_decay_steps: int = 100_000
   beta_start: float = 1.0
   beta_end: float = 0.01
   beta_decay_steps: int = 100_000
+  eps_start: float = 1.0
+  eps_end: float = 0.05
+  eps_decay_steps: int = 50_000
 
 
   # Transformer architecture params
   state_shape: Tuple[int, int, int] = None  # (H, W, F)
   H: int = 7  # grid height
   W: int = 7  # grid width
-  state_feature_splits: Tuple[int, ...] = ()
   # for DiscreteActionEmbedder - !at least one mustn't be None!
   action_dim: int = 4
-  # for ActionEmbeddings - !at least one mustn't be None!
-  action_feature_splits: Tuple[int, ...] = None
-  latent_dim: int = 32
   d_model: int = 256
   nhead: int = 4
   num_encoder_layers: int = 1
-  num_decoder_layers: int = 1
   dim_feedforward: int = 1024
   dropout: float = 0.1
