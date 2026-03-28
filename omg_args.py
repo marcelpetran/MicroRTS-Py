2d464d97501312645d341fa2025bc201473d8c93
marcelpetran <marcelpetran55@gmail.com>
2026-03-28 09:32:01 +0100

    Refactor OMGArgs parameters for improved training efficiency and stability

--- omg_args.py
+++ omg_args.py
@@ -8,10 +8,9 @@ class OMGArgs:
   gamma: float = 0.98
   lr: float = 5e-5
   batch_size: int = 128
-  capacity: int = 10_000
-  min_replay: int = 1_000
-  train_every: int = 4
-  target_update_every: int = 1_000
+  capacity: int = 1_000
+  min_replay: int = 256
+  train_every: int = 2
   visualise_every_n_step: int = 3
   qnet_hidden: int = 256
   cnn_hidden: int = 64
@@ -21,13 +20,13 @@ class OMGArgs:
   folder_id: int = 0
 
   oracle: bool = False
-  tau_soft: float = 0.005
-  tau_start: float = 2.1
-  tau_end: float = 0.01
-  tau_decay_steps: int = 300_000
+  tau_soft: float = 0.001
+  tau_start: float = 2.0
+  tau_end: float = 0.05
+  tau_decay_steps: int = 600_000
   beta_start: float = 1.0
   beta_end: float = 0.01
-  beta_decay_steps: int = 300_000
+  beta_decay_steps: int = 50_000
   eps_start: float = 1.0
   eps_end: float = 0.05
   eps_decay_steps: int = 50_000
